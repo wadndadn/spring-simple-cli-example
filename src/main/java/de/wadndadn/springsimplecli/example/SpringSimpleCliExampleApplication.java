0108c7bbcc8d5094645bbc3c38c8314149bcde6e
Christian Schubert <544196+SchubertCh@users.noreply.github.com>
2023-01-17 12:37:36 +0100

    Inital commit

---
+++ src/main/java/de/wadndadn/springsimplecli/example/SpringSimpleCliExampleApplication.java
@@ -0,0 +1,13 @@
+package de.wadndadn.springsimplecli.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleCliExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSimpleCliExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/wadndadn/springsimplecli/example/SpringSimpleCliExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wadndadn.springsimplecli.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleCliExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
